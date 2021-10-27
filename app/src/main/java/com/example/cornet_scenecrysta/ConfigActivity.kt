17f054c65a7c22518c511360fbad1482a992890c
HisuiAoi <kenpeidayo@gmail.com>
2021-10-27 13:05:07 +0900

    Create Activity/Layout

---
+++ app/src/main/java/com/example/cornet_scenecrysta/ConfigActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cornet_scenecrysta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfigActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.config_activity)
+    }
+}
