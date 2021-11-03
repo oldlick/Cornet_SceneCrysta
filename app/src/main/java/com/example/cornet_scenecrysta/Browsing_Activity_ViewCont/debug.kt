b35665bd62240d9158d12331ff223ff44cb385cf
HisuiAoi <kenpeidayo@gmail.com>
2021-11-03 23:28:56 +0900

    del debug.kt

--- app/src/main/java/com/example/cornet_scenecrysta/Browsing_Activity_ViewCont/debug.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cornet_scenecrysta.Browsing_Activity_ViewCont
-
-import android.content.Context
-import android.widget.Toast
-
-fun toast(context: Context, message:String){
-    Toast.makeText(context,message,Toast.LENGTH_SHORT).show()
-}
